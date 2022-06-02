21bf30d2ed1b10a7e6582a41248576b1b34e783c
MaelKocab <mael.kocab@gmail.com>
2022-06-02 11:17:19 +0200

    API Connected

--- beerscanfrontend/rectangle.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-def rectangle(image, request_results):
-    color = (0, 255, 0)
-    for elem in request_results["boxes"].values():
-        cv2.rectangle(image, (elem['startX'], elem['startY']),
-                      (elem['endX'], elem['endY']), color, 2)
-    return image
